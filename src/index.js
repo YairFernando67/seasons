868f3ec366469a9a659ac5ad8ee79750ca755720
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2019-12-01 17:41:22 -0600

    Improved the render method by getting rid of the if statements and adding a helper method

--- src/index.js
+++ src/index.js
@@ -23,8 +23,7 @@ class App extends React.Component {
     );
   }
 
-  //React says that we have to define render!!
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -34,6 +33,15 @@ class App extends React.Component {
     }
 
     return <Spinner message="Please accept location request" />;
+  }
+
+  //React says that we have to define render!!
+  render() {
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    )
   };
 };
 
